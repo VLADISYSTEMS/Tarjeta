719bdb82211dbc09c3655630bb755aaf59f47dd0
VLADISYSTEMS <vladiblues1969@gmail.com>
2026-06-29 20:30:13 -0500

    Update App.jsx
    CAMBIO DE LINKS OPARA QR -->ENTRA PORTFOLIO

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,24 @@ import React from "react";
 import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
 import Logo from "../public/LogoVladisystems.png"; // ajusta la ruta si es necesario
 import "./index.css";
-
-
 export default function App() {
   return (
-    <div className="app-container">
-      <img src={Logo} alt="Logo Vladisystems" className="logo" />
-      <h1>Vladisystems</h1>
-      <p>
-        Desarrollo y Automatización | Sistemas en COBOL, Python y Web | UI/UX
-        & Automatización
-      </p>
-      <div className="icons">
-        <a href="https://www.linkedin.com/in/vladimir-alba-964931246/" target="_blank">
-          <FaLinkedin />
-        </a>
-        <a href="https://github.com/VLADISYSTEMS" target="_blank">
-          <FaGithub />
-        </a>
-        <a href="https://www.instagram.com/vladisystems" target="_blank">
-          <FaInstagram />
-        </a>
-      </div>
-    </div>
-  );
-}
+  <iframe
+    src="https://vladisystemsportfolio.netlify.app/"
+    title="Vladisystems Portfolio"
+    style={{
+      width: "100%",
+      height: "100vh",
+      border: "none",
+      margin: 0,
+      padding: 0,
+    }}
+  />
+);
+
+
+
+  
 
 
 
